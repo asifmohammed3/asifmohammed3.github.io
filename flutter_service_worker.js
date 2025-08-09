d7bc8f64353c492ef0b4831f59d4177e6d2f1b07
asifmohammed3 <asifmohammed3@users.noreply.github.com>
2025-08-09 19:27:54 +0000

    deploy: 12e25d1e22254c1982d4899dc2433ddf9f484d38

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "8f42475801e77a251046a2249745b086",
+const RESOURCES = {"flutter_bootstrap.js": "f77bc099e87c42805baf5ea2e3111d67",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
